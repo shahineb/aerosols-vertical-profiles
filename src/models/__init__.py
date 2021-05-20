0f114c335cd05a7072442cb045c0a534f21c20a3
shahine <shahine96@gmail.com>
2021-05-20 19:22:54 +0100

    changed aggregation to sum in ridge regression + added transform ridge regression:

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,7 @@
-from .ridge_regression import AggregateRidgeRegression
+from .ridge_regression import AggregateRidgeRegression, TransformedAggregateRidgeRegression
 from .kernel_ridge_regression import AggregateKRR, ConditionalAggregateKRR, TransformedConditionalAggregateKRR
 from .variational_gp import VariationalCMP
 
-__all__ = ['AggregateKRR', 'ConditionalAggregateKRR',
-           'AggregateRidgeRegression', 'TransformedConditionalAggregateKRR',
+__all__ = ['AggregateKRR', 'ConditionalAggregateKRR', 'TransformedConditionalAggregateKRR',
+           'AggregateRidgeRegression', 'TransformedAggregateRidgeRegression',
            'VariationalCMP']
--- src/models/ridge_regression.py
+++ src/models/ridge_regression.py
@@ -10,11 +10,26 @@ class AggregateRidgeRegression(nn.Module):
 
     Args:
         alpha (float): regularization weight, greater = stronger L2 penalization
+        fit_intercept (bool): if True, pads inputs with constant offset
     """
 
-    def __init__(self, alpha):
+    def __init__(self, alpha, fit_intercept=False):
         super().__init__()
         self.alpha = alpha
+        self.fit_intercept = fit_intercept
+
+    def pad_input(self, x):
+        """Pads x with 1 along last dimension
+
+        Args:
+            x (torch.Tensor)
+
+        Returns:
+            type: torch.Tensor
+
+        """
+        x = torch.cat([x, torch.ones(x.shape[:-1], device=x.device).unsqueeze(-1)], dim=-1)
+        return x
 
     def fit(self, individuals_covariates, aggregate_targets):
         """Fits model following sklearn syntax
@@ -25,10 +40,12 @@ class AggregateRidgeRegression(nn.Module):
             aggregate_targets (torch.Tensor): (n_bags,) of aggregate targets observed for each bag
 
         """
+        if self.fit_intercept:
+            individuals_covariates = self.pad_input(individuals_covariates)
         n_bags = individuals_covariates.size(0)
         d = individuals_covariates.size(-1)
 
-        EXy = individuals_covariates.mean(dim=1).t()
+        EXy = individuals_covariates.sum(dim=1).t()
         Q = EXy @ EXy.t() + n_bags * self.alpha * torch.eye(d)
 
         beta = gpytorch.inv_matmul(Q, EXy @ aggregate_targets)
@@ -45,4 +62,77 @@ class AggregateRidgeRegression(nn.Module):
             type: torch.Tensor
 
         """
+        if self.fit_intercept:
+            x = self.pad_input(x)
         return x @ self.beta
+
+
+class TransformedAggregateRidgeRegression(nn.Module):
+    """Short summary.
+
+    Args:
+        transform (callable): output transformation to apply to prediction
+        alpha (float): regularization weight, greater = stronger L2 penalization
+        ndim (int): dimensionality of inputs
+        fit_intercept (bool): if True, pads inputs with constant offset
+
+    """
+    def __init__(self, transform, alpha, ndim, fit_intercept=False):
+        super().__init__()
+        self.transform = transform
+        self.alpha = alpha
+        self.fit_intercept = fit_intercept
+        self.ndim = ndim
+        if self.fit_intercept:
+            self.ndim += 1
+        self.beta = nn.Parameter(torch.rand(self.ndim))
+
+    def pad_input(self, x):
+        """Pads x with 1 along last dimension
+
+        Args:
+            x (torch.Tensor)
+
+        Returns:
+            type: torch.Tensor
+
+        """
+        x = torch.cat([x, torch.ones(x.shape[:-1], device=x.device).unsqueeze(-1)], dim=-1)
+        return x
+
+    def forward(self, x):
+        """Runs prediction
+
+        Args:
+            x (torch.Tensor): (n_samples, covariate_dimenionality)
+                samples must not need to be organized by bags
+
+        Returns:
+            type: torch.Tensor
+
+        """
+        if self.fit_intercept:
+            x = self.pad_input(x)
+        return self.transform(x @ self.beta)
+
+    def aggregate_prediction(self, prediction):
+        """Computes aggregation of individuals output prediction
+
+        Args:
+            prediction (torch.Tensor): (n_bag, bags_size) tensor output of forward
+
+        Returns:
+            type: torch.Tensor
+
+        """
+        aggregate_prediction = prediction.sum(dim=1)
+        return aggregate_prediction
+
+    def regularization_term(self):
+        """Square L2 norm of beta
+
+        Returns:
+            type: torch.Tensor
+
+        """
+        return self.alpha * torch.dot(self.beta, self.beta)
