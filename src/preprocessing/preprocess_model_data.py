aa31a3e90d5b0fd3791ba1c05d9b225a7e8d1d4f
sofstef <sof.stefanovic@gmail.com>
2021-05-29 17:35:52 +0100

    testing covariates and params

--- src/preprocessing/preprocess_model_data.py
+++ src/preprocessing/preprocess_model_data.py
@@ -6,9 +6,11 @@ standardize = lambda x: (x - x.mean()) / x.std()
 
 def load_dataset(file_path, trimming_altitude_idx):
     # Load dataset
-    dataset = xr.open_dataset(file_path).isel(lat=slice(200, 350),
-                                              lon=slice(200, 350),
-                                              time=slice(0, 3))
+    dataset = xr.open_dataset(file_path)
+    
+#     .isel(lat=slice(200, 350),
+#                                               lon=slice(200, 350),
+#                                               time=slice(0, 3))
 
     # Trim altitude
     dataset = dataset.isel(lev=slice(trimming_altitude_idx, len(dataset.lev)))
